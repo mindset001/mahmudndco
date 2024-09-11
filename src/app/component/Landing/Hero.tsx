3759b0e56391fc79316d5b058aefe45ddd901e7d
shakiru102 <ashakiru53@gmail.com>
2024-09-11 08:52:25 +0100

    hero-new

--- src/app/component/Landing/Hero.tsx
+++ src/app/component/Landing/Hero.tsx
@@ -9,12 +9,12 @@ import localFont from 'next/font/local'
 function Hero() {
   return (
   <main className='w-full hero mt-[-60px] text-[#fff]'>
-    <div className='flex justify-end items-end h-[80%] px-10'>
+    <div className='flex justify-start md:justify-end items-end h-[80%] px-10'>
       <div>
       <div>
        <h1 className='text-[44px]'>  Finest Legal Advisory.</h1>
        </div>
-        <div className='flex justify-end mt-4'>
+        <div className='flex justify-start md:justify-end mt-4'>
             <Image src={Left} alt='' className='w-[48px] h-[48px]'/>
             <Image src={Right} alt='' className='w-[48px] h-[48px] ml-[-10px]'/>
         </div>
